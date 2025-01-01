0e0ce7f63009f02a55c4f99ad7c15b32b81cfac2
Renato <renato.j.olveira@gmail.com>
2025-01-01 14:09:35 -0300

    i don	 remember hahah

--- projeto_receita/urls.py
+++ projeto_receita/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def my_home(request):
+    return HttpResponse('<h1>Hello Amigo</h1>')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', my_home),
 ]
